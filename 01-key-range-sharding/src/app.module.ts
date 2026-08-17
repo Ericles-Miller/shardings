7795d7161dcf5877dcafc3b246b2cced33f79e85
Éricles Miller <ericlesmiller15@gmail.com>
2026-08-17 16:59:03 -0300

    feat: create a new connections of database

--- 01-key-range-sharding/src/app.module.ts
+++ 01-key-range-sharding/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createShardConnection } from './database/create-shard-connection';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(
+      createShardConnection('db_a', 'DB_A_HOST', 'DB_A_PORT'),
+    ),
+    TypeOrmModule.forRootAsync(
+      createShardConnection('db_b', 'DB_B_HOST', 'DB_B_PORT'),
+    ),
+    TypeOrmModule.forRootAsync(
+      createShardConnection('db_c', 'DB_C_HOST', 'DB_C_PORT'),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
